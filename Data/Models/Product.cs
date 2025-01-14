8a4001702c5c8d9c69bd1c425c8f6a085b359942
JoffreArriag <joffrearriagat16@gmail.com>
2025-01-13 19:18:09 -0500

    mas cambios

--- Data/Models/Product.cs
+++ Data/Models/Product.cs
@@ -10,10 +10,10 @@ namespace Data.Models
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-        public string ImageUrl { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public string Imagen { get; set; } = string.Empty;
         public int CategoryId { get; set; }
         public bool IsActive { get; set; }
 
--- apiPartyStore/Controllers/ProductController.cs
+++ apiPartyStore/Controllers/ProductController.cs
@@ -7,29 +7,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Data.Contexts;
 using Data.Models;
-using System.Reflection;
 
 namespace apiPartyStore.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductController : ControllerBase
     {
         private readonly PartyContext _context;
 
-        public ProductsController(PartyContext context)
+        public ProductController(PartyContext context)
         {
             _context = context;
         }
 
-        // GET: api/Products
+        // GET: api/Product
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
             return await _context.Products.ToListAsync();
         }
 
-        // GET: api/Products/5
+        // GET: api/Product/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
@@ -43,7 +42,7 @@ namespace apiPartyStore.Controllers
             return product;
         }
 
-        // PUT: api/Products/5
+        // PUT: api/Product/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutProduct(int id, Product product)
@@ -52,6 +51,7 @@ namespace apiPartyStore.Controllers
             {
                 return BadRequest();
             }
+
             _context.Entry(product).State = EntityState.Modified;
 
             try
@@ -73,7 +73,7 @@ namespace apiPartyStore.Controllers
             return NoContent();
         }
 
-        // POST: api/Products
+        // POST: api/Product
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Product>> PostProduct(Product product)
@@ -84,7 +84,7 @@ namespace apiPartyStore.Controllers
             return CreatedAtAction("GetProduct", new { id = product.Id }, product);
         }
 
-        // DELETE: api/Products/5
+        // DELETE: api/Product/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProduct(int id)
         {
