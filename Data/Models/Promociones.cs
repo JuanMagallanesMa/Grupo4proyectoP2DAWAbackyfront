daf0964edc4e77bddfce480f7181ce471cd004b0
JuanMagallanesMa <122581946+JuanMagallanesMa@users.noreply.github.com>
2025-01-20 12:35:55 -0500

    cmb

--- Data/Models/Promociones.cs
+++ Data/Models/Promociones.cs
@@ -14,7 +14,7 @@ namespace Data.Models
         public string? Descripcion { get; set; }
 
         public int? Id_categoria { get; set; }
-        [JsonIgnore]
+        
         public virtual Category? Categoria { get; set; } = null!; // Navegación a la entidad Categoria
        
         public decimal Descuento_porcentaje { get; set; }
