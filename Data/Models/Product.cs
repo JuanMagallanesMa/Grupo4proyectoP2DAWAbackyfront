d94e969cc9c655ea33dc9a3b7b19180611254b4f
JoffreArriag <joffrearriagat16@gmail.com>
2025-01-21 18:23:56 -0500

    cambios

--- Data/Models/Product.cs
+++ Data/Models/Product.cs
@@ -11,12 +11,10 @@ namespace Data.Models
     public class Product
     {
         public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public string Descripcion { get; set; } = string.Empty;
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
         public decimal Precio { get; set; }
-
-        [Column("Imagen")]
-        public string Imagen { get; set; } = string.Empty;
+        public string Imagen { get; set; } 
 
         public int CategoryId { get; set; }
         public bool IsActive { get; set; }
