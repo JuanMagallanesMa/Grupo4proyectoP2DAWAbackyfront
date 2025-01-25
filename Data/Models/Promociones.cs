c254373edb2e20256d8165a53c8de5769e835a74
Erickabs <ericka.bonetes@ug.edu.ec>
2025-01-24 21:18:40 -0500

    backend

--- Data/Models/Promociones.cs
+++ Data/Models/Promociones.cs
@@ -15,9 +15,9 @@ namespace Data.Models
         public int? Id_categoria { get; set; }
         public Category? Categoria { get; set; } // Navegación a la entidad Categoria
        
-        public decimal Descuento_porcentaje { get; set; }
+        public decimal DescuentoPorcentaje { get; set; }
         
-        public DateTime Fecha_fin { get; set; }
+        public DateTime FechaFin { get; set; }
         public bool IsActive { get; set; }
     }
 }
